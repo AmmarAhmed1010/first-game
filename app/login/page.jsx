68d85822ca393229668af695792521b30f004d56
Ammar Ahmed <ammar7298@gmail.com>
2026-08-11 12:47:36 +0500

    fa

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 import { useAuth } from '@/context/AuthContext';
 import { Eye, EyeOff, Gamepad2, Loader2, Zap ,Lightning } from 'lucide-react';
 export default function LoginPage() {
+  //3213213213
   const router   = useRouter();
   const { user, login } = useAuth();
   const [email, setEmail]       = useState('');
